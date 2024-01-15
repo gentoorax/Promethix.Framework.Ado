22a484f7f46dba3ba49a70213842ae55e3dea221
Christopher Law <chris@chrislaw.me>
2024-01-15 15:22:25 +0000

    Create test to identify problem with Dapper non-scalar transactional reads.

---
+++ Promethix.Framework.Ado.Tests/TestSupport/DataAccess/Mssql/MssqlContextExample3.cs
@@ -0,0 +1,23 @@
+﻿using Promethix.Framework.Ado.Enums;
+using Promethix.Framework.Ado.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promethix.Framework.Ado.Tests.TestSupport.DataAccess.Mssql
+{
+    public class MssqlContextExample3 : AdoContext
+    {
+        public MssqlContextExample3()
+            : base(
+                "MssqlContextExample3",
+                "Microsoft.Data.SqlClient",
+                "Server=(local);Database=AdoScopeTest1;Integrated Security=True;TrustServerCertificate=True",
+                AdoContextExecutionOption.Transactional)
+        {
+            // No Implementation
+        }
+    }
+}
